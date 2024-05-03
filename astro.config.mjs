60b240543ab94f6f258df0808bbad1e1e28f30ab
pietrodev07 <pietro.dev.07@gmail.com>
2024-05-03 16:08:32 +0200

    chore: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), mdx(), tailwind(), icon()],
+});
