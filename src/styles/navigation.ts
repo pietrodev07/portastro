b66fc168c70b609bfc79c2e5fb4ec0d0a4b6bda6
pietrodev07 <pietro.dev.07@gmail.com>
2024-05-04 14:55:04 +0200

    feat: create navigation component

---
+++ src/styles/navigation.ts
@@ -0,0 +1,12 @@
+export const navigationClass = {
+  navbar: [
+    "not-prose",
+    "m-auto",
+    "w-full",
+    "max-w-[65ch]",
+    "pb-4",
+    "text-zinc-700",
+    "dark:text-white/80",
+  ],
+  text: ["text-2xl", "font-extrabold", "text-zinc-900", "dark:text-white"],
+};
