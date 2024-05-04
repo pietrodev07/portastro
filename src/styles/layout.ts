0ef131d04432ee8412f19557e91f11cb3cb949b0
pietrodev07 <pietro.dev.07@gmail.com>
2024-05-04 15:37:56 +0200

    feat: create main layout

---
+++ src/styles/layout.ts
@@ -0,0 +1,15 @@
+export const layoutClass = {
+  body: ["bg-white", "dark:bg-zinc-900", "dark:text-white", "px-6", "py-14"],
+  main: ["prose", "prose-zinc", "m-auto", "dark:prose-invert"],
+  backButton: [
+    "rounded-full",
+    "border",
+    "bg-zinc-50",
+    "px-2",
+    "font-bold",
+    "text-zinc-700",
+    "dark:border-zinc-700",
+    "dark:bg-zinc-800",
+    "dark:text-white",
+  ],
+};
